069e68c912491da24d4c2c7a9773ae085568ec51
Omid Ziveh <omidziveh1386@gmail.com>
2021-09-27 23:00:59 +0330

    slidable returned

--- lib/db/models/pre_task/pretask_widget.dart
+++ lib/db/models/pre_task/pretask_widget.dart
@@ -23,7 +23,14 @@ class ShowTile extends StatelessWidget {
         child: tile(
           title: this.preTask.title,
           importance: this.preTask.importance,
-          actions: [deleteOption(preTask, box)],
+          actions: [
+            GestureDetector(
+              onTap: () {
+                print('tap');
+              },
+              child: Container(),
+            ),
+          ],
           secondActions: [weekOption(preTask, box)],
         ),
       ),
@@ -37,34 +44,53 @@ Widget tile({
   List<Widget>? actions,
   List<Widget>? secondActions,
 }) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(10, 15, 10, 15),
+  return Container(
+    height: 60,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10),
+      border: Border.all(
+        color: Colors.black,
+        width: 1,
+      ),
+    ),
     child: Slidable(
-      //actionExtentRatio: 1 / 6,
+      actionExtentRatio: 1 / 6,
       actionPane: SlidableBehindActionPane(),
       actions: actions,
-      //secondaryActions: secondActions,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(title, style: captureToolPreTaskTitle),
-          Spacer(),
-          importance > 0 ? activeImportanceIcon : deactiveImportanceIcon,
-          importance > 1 ? activeImportanceIcon : deactiveImportanceIcon,
-          importance > 2 ? activeImportanceIcon : deactiveImportanceIcon,
-        ],
+      secondaryActions: secondActions,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                importance > 0 ? activeImportanceIcon : deactiveImportanceIcon,
+                importance > 1 ? activeImportanceIcon : deactiveImportanceIcon,
+                importance > 2 ? activeImportanceIcon : deactiveImportanceIcon,
+                Spacer(),
+                Text(
+                  title,
+                  style: captureToolPreTaskTitle,
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     ),
   );
 }
 
 class PreTaskOption extends StatelessWidget {
-  PreTask preTask;
   Color color;
-  Icon icon;
+  IconData icon;
   Function() onTap;
   PreTaskOption(
-    this.preTask,
     this.color,
     this.icon,
     this.onTap,
@@ -73,18 +99,13 @@ class PreTaskOption extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(right: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
+      padding: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: IconSlideAction(
+          icon: this.icon,
           color: this.color,
-        ),
-        child: Center(
-          child: IconButton(
-            icon: this.icon,
-            splashColor: Colors.transparent,
-            onPressed: this.onTap(),
-          ),
+          onTap: this.onTap,
         ),
       ),
     );
@@ -93,9 +114,8 @@ class PreTaskOption extends StatelessWidget {
 
 Widget deleteOption(PreTask preTask, Box startBox) {
   return PreTaskOption(
-    preTask,
     Colors.red,
-    Icon(Icons.delete),
+    Icons.delete,
     () {
       print('deleted');
       delete_pretask(preTask.id, startBox);
@@ -105,9 +125,8 @@ Widget deleteOption(PreTask preTask, Box startBox) {
 
 Widget monthOption(PreTask preTask, Box startBox) {
   return PreTaskOption(
-    preTask,
     Colors.blue,
-    Icon(Icons.archive_outlined),
+    Icons.archive_outlined,
     () {
       print('go to monthly');
       go_to_monthly(preTask.id, startBox);
@@ -117,9 +136,8 @@ Widget monthOption(PreTask preTask, Box startBox) {
 
 Widget weekOption(PreTask preTask, Box startBox) {
   return PreTaskOption(
-    preTask,
     Colors.green,
-    Icon(Icons.pending_actions_rounded),
+    Icons.pending_actions_rounded,
     () {
       print('go to weekly');
       go_to_weekly(preTask.id, startBox);
@@ -129,9 +147,8 @@ Widget weekOption(PreTask preTask, Box startBox) {
 
 Widget captureOption(PreTask preTask, Box startBox) {
   return PreTaskOption(
-    preTask,
     Colors.green,
-    Icon(Icons.toc_rounded),
+    Icons.toc_rounded,
     () {
       go_to_weekly(preTask.id, startBox);
     },
